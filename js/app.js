0027ea20d4f731b13e761ddf787cb162c82ba1c1
Matt Pahuta <mhpahuta@gmail.com>
2023-01-26 15:11:55 -0800

    Stage post data via JS, wire script file to all pages

---
+++ js/app.js
@@ -0,0 +1,7 @@
+console.log('app.js connected!')
+
+const loadPostsBtn = document.querySelector('#load-posts');
+
+loadPostsBtn.addEventListener('click', () => {
+  console.log('loading more posts...')
+})
